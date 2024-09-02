73214ffdc9e443b6be0e9a7b1586314c27ae3ad5
liikalaj <30653349+liikalaj@users.noreply.github.com>
2024-09-02 09:08:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function calculate() {
-    const itemValue = parseFloat(document.getElementById('item').value);
-    const percentage = parseFloat(document.getElementById('percentage').value);
-    const result = (itemValue * percentage) / 100;
-    document.getElementById('result').innerText = `Result: ${result}`;
+    for (let i = 1; i <= 5; i++) {
+        const itemValue = parseFloat(document.getElementById(`item${i}`).value);
+        const percentage = parseFloat(document.getElementById(`percentage${i}`).value);
+        const result = (itemValue * percentage) / 100;
+        document.getElementById(`result${i}`).innerText = `Result: ${result}`;
+    }
 }
