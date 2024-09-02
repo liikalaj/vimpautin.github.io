937a02aa55a606fff49b10f6821839da39ceb05a
liikalaj <30653349+liikalaj@users.noreply.github.com>
2024-09-02 10:44:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function calculate() {
     for (let i = 1; i <= 5; i++) {
         const itemValue = parseFloat(document.getElementById(`item${i}`).value);
         const percentage = parseFloat(document.getElementById(`percentage${i}`).value);
-        const result = (itemValue * percentage) / 100;
-        document.getElementById(`result${i}`).innerText = `Annos: ${result} grammaa`;
+        const resultElement = document.getElementById(`result${i}`);
+        
+        if (percentage === 0) {
+            resultElement.innerText = '';
+        } else {
+            const result = (itemValue * percentage) / 100;
+            resultElement.innerText = `Result: ${result}`;
+        }
     }
 }
